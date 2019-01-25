4d4a80de781d5df8b0080f6be4c36211ded03002
Kenneth Bruce <k.bruce16@gmail.com>
2019-01-25 08:46:57 -0500

    fixed fortran

--- lab1/fortran.f95
+++ lab1/fortran.f95
@@ -1,13 +1,13 @@
-!How to loop in fortran
+!Fortran loop
 program testloop
 integer :: i, j, k
 j = 15
 k = (j + 13) / 27
-do while (k < 10)
+do while (k <= 10)
     k = k + 1
     i = 3 * k - 1
     print *, 'i = ',i
-    print *, 'j = ',j
     print *, 'k = ',k
+    print *, 'j = ',j
 end do
 end program testloop
