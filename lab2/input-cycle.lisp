033e7e9929059b6b661809d36c04d88225359f57
Kenneth Bruce <kbruce7@localhost.localdomain>
2019-02-28 22:28:18 -0500

    good enough

--- lab2/input-cycle.lisp
+++ lab2/input-cycle.lisp
@@ -7,30 +7,25 @@
 (defun generate-clue ()
   (format nil "pretend this is a clue"))
 
-(defun main-loop (num-players)
-  (loop
-     (let ((player-list (loop for y from 1 to num-players collecting y)))
+w(defun main-loop (num-players)
+  (let ((player-list (loop for y from 1 to num-players collecting y)) (black-list nil))
+    (loop
        (loop for x in player-list do
 	    (cond
-	      ((member x player-list) (progn
-				    (format t "Player ~a, it is now your turn.~%" x)
-				    (prog ((player-choice (prompt-read "Press ENTER to get your clue or type GUESS to try to guess.")))
-				       (cond
-					 ((equalp player-choice "GUESS")
-					  (progn
-					    (cond
-					      ((y-or-n-p "Were you successful? ") (return-from main-loop 'GAME_OVER))
-					      (t (print (setf player-list (remove x player-list)))))))
-					 (t
-					  (progn
-					    (format t "~%~a~%" (generate-clue))
-					    (prompt-read "Press ENTER when you are done.")
-					    (format t "~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%")))))))
+	      ((not (member x black-list))
+	       (progn
+		 (format t "Player ~a, it is now your turn.~%" x)
+		 (prog ((player-choice (prompt-read "Press ENTER to get your clue or type GUESS to try to guess.")))
+		    (cond
+		      ((equalp player-choice "GUESS")
+		       (progn
+			 (cond
+			   ((y-or-n-p "Were you successful? ") (return-from main-loop 'GAME_OVER))
+			   (t (push x black-list)))))
+		      (t
+		       (progn
+			 (format t "~%~a~%" (generate-clue))
+			 (prompt-read "Press ENTER when you are done.")
+			 (format t "~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%~%")))))))
+	      ((equalp (list-length black-list) (list-length player-list)) (return-from main-loop 'GAME_OVER))
 	      (t nil))))))
-
-(defun a-loop-test (the-list)
-  (loop for x in the-list do
-       (progn
-	 (print the-list)
-	 (print x)
-	 (setf the-list (remove (+ x 1) the-list)))))
