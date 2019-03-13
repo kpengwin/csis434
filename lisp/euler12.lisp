40a52f43a2b7e5f6a1c80b4c922dee2e38c69f9a
Kenneth Bruce <me@kenneth-bruce.com>
2019-03-13 17:58:11 -0400

    much faster euler12

--- lisp/euler12.lisp
+++ lisp/euler12.lisp
@@ -4,14 +4,16 @@
     (t nil)))
 
 (defun count-divisors (number)
-  (prog ((num-divisors 0))
-    (loop for x from 1 to number do
-	 (cond
-	   ((divides x number) (setf num-divisors (+ 1 num-divisors)))))
-    (return-from count-divisors num-divisors)))
+  (prog ((num-divisors 2))
+     (cond
+       ((= (sqrt number) (isqrt number)) (setf num-divisors (- num-divisors 1))))
+     (loop for x from 2 to (isqrt number) do
+	  (cond
+	    ((divides x number) (setf num-divisors (+ 2 num-divisors)))))
+     (return-from count-divisors num-divisors)))
 
 (defun answer12 ()
-  (let ((x 1) (triangular-num 0) (highest-digits 0))
+  (let ((x 2) (triangular-num 1) (highest-digits 1))
     (loop
        (prog ((num-divisors (count-divisors triangular-num)))
 	  (cond ((> num-divisors highest-digits)
